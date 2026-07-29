e6870d6078c3b731585f20c16747e5ca536f7063
Rikasad <154080805+Rikasad@users.noreply.github.com>
2026-07-29 14:34:18 +0600

    Add files via upload
    oop problem 01 lab

---
+++ division.java
@@ -0,0 +1,8 @@
+public class division {
+        public static void main(String[] args){
+      int num1 = 50;
+        int num2 = 3;
+        int sum = num1 / num2;
+        System.out.println(sum);
+    }
+}
